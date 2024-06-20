b3f725174dfa56e6ad9171348673e081a50b5e2f
Danielhndzfor <87097302+Danielhndzfor@users.noreply.github.com>
2024-06-19 22:52:42 -0600

    Reparando rutas

--- src/pages/Especialidades.jsx
+++ src/pages/Especialidades.jsx
@@ -1,5 +1,6 @@
 import Encabezado from "../components/Encabezado";
 import '../css/Especialidades.css';
+import { Link } from 'react-router-dom';
 
 function Especialidades() {
     return (
@@ -12,7 +13,7 @@ function Especialidades() {
                 </div>
 
                 <div className="tipos">
-                    <a href="colima" className="esp">
+                    <Link to="/colima" className="esp">
                         <p className="espOpc">COLIMA</p>
                         <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-arrow-right" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -21,9 +22,9 @@ function Especialidades() {
                             <path d="M16 12h-8" />
                             <path d="M12 16l4 -4" />
                         </svg>
-                    </a>
+                    </Link>
 
-                    <a href="honolulu" className="esp">
+                    <Link to="/honolulu" className="esp">
                         <p className="espOpc">HONOLULU</p>
                         <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-arrow-right" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -32,9 +33,9 @@ function Especialidades() {
                             <path d="M16 12h-8" />
                             <path d="M12 16l4 -4" />
                         </svg>
-                    </a>
+                    </Link>
 
-                    <a href="sinnombre" className="esp">
+                    <Link to="/sinnombre" className="esp">
                         <p className="espOpc">SIN NOMBRE</p>
                         <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-arrow-right" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -43,9 +44,9 @@ function Especialidades() {
                             <path d="M16 12h-8" />
                             <path d="M12 16l4 -4" />
                         </svg>
-                    </a>
+                    </Link>
 
-                    <a href="quemenotas" className="esp">
+                    <Link to="/quemenotas" className="esp">
                         <p className="espOpc">QUE ME NOTAS</p>
                         <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-arrow-right" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -54,9 +55,9 @@ function Especialidades() {
                             <path d="M16 12h-8" />
                             <path d="M12 16l4 -4" />
                         </svg>
-                    </a>
+                    </Link>
 
-                    <a href="alambrepollo" className="esp">
+                    <Link to="/alambrepollo" className="esp">
                         <p className="espOpc">ALAMBRE DE POLLO</p>
                         <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-arrow-right" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -65,9 +66,9 @@ function Especialidades() {
                             <path d="M16 12h-8" />
                             <path d="M12 16l4 -4" />
                         </svg>
-                    </a>
+                    </Link>
 
-                    <a href="alambrexl" className="esp">
+                    <Link to="/alambrexl" className="esp">
                         <p className="espOpc">ALAMBRE XL</p>
                         <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-arrow-right" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -76,9 +77,9 @@ function Especialidades() {
                             <path d="M16 12h-8" />
                             <path d="M12 16l4 -4" />
                         </svg>
-                    </a>
+                    </Link>
 
-                    <a href="atuncito" className="esp">
+                    <Link to="/atuncito" className="esp">
                         <p className="espOpc">ATUNCITO</p>
                         <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-arrow-right" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -87,9 +88,9 @@ function Especialidades() {
                             <path d="M16 12h-8" />
                             <path d="M12 16l4 -4" />
                         </svg>
-                    </a>
+                    </Link>
 
-                    <a href="mexico" className="esp">
+                    <Link to="/mexico" className="esp">
                         <p className="espOpc">MÉXICO</p>
                         <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-arrow-right" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -98,9 +99,9 @@ function Especialidades() {
                             <path d="M16 12h-8" />
                             <path d="M12 16l4 -4" />
                         </svg>
-                    </a>
+                    </Link>
 
-                    <a href="yatevi" className="esp">
+                    <Link to="/yatevi" className="esp">
                         <p className="espOpc">YA TE VI</p>
                         <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-arrow-right" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -109,9 +110,9 @@ function Especialidades() {
                             <path d="M16 12h-8" />
                             <path d="M12 16l4 -4" />
                         </svg>
-                    </a>
+                    </Link>
 
-                    <a href="taquiqueso" className="esp">
+                    <Link to="/taquiqueso" className="esp">
                         <p className="espOpc">TAQUIQUESO</p>
                         <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-arrow-right" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -120,9 +121,9 @@ function Especialidades() {
                             <path d="M16 12h-8" />
                             <path d="M12 16l4 -4" />
                         </svg>
-                    </a>
+                    </Link>
 
-                    <a href="sinaloa" className="esp">
+                    <Link to="/sinaloa" className="esp">
                         <p className="espOpc">SINALOA</p>
                         <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-arrow-right" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -131,7 +132,7 @@ function Especialidades() {
                             <path d="M16 12h-8" />
                             <path d="M12 16l4 -4" />
                         </svg>
-                    </a>
+                    </Link>
 
                 </div>
             </div>
